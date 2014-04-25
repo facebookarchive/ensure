09a9c2982421cd24609705c0e222008b0781457d
Naitik Shah <n@daaku.org>
2014-04-25 09:13:14 -0700

    initial commit

---
+++ ensure.go
@@ -0,0 +1,222 @@
+// Package ensure provides utilities for testing to ensure the
+// given conditions are met and Fatal if they aren't satisified.
+//
+// The various functions here show a useful error message automatically
+// including identifying source location. They additionally support arbitary
+// arguments which will be printed using the spew library.
+package ensure
+
+import (
+	"bytes"
+	"fmt"
+	"path/filepath"
+	"reflect"
+	"regexp"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/facebookgo/stack"
+	subsetp "github.com/facebookgo/subset"
+)
+
+// Fataler defines the minimal interface necessary to trigger a Fatal when a
+// condition is hit. testing.T & testing.B satisfy this for example.
+type Fataler interface {
+	Fatal(a ...interface{})
+}
+
+// cond represents a condition that wasn't satisfied, and is useful to generate
+// log messages.
+type cond struct {
+	Fataler    Fataler
+	Skip       int
+	Format     string
+	FormatArgs []interface{}
+	Extra      []interface{}
+}
+
+// This deletes "ensure.go:xx" removing a confusing piece of information since
+// it will be an internal reference.
+var deleteSelf = strings.Repeat("\b", 15)
+
+func (c cond) String() string {
+	var b bytes.Buffer
+	fmt.Fprintf(&b, "%s%s", deleteSelf, pstack(stack.Callers(c.Skip+1)))
+	if c.Format != "" {
+		fmt.Fprintf(&b, c.Format, c.FormatArgs...)
+	}
+	if len(c.Extra) != 0 {
+		fmt.Fprint(&b, "\n")
+		fmt.Fprint(&b, tsdump(c.Extra...))
+	}
+	return b.String()
+}
+
+// fatal triggers the fatal and logs the cond's message. It adds 2 to Skip, to
+// skip itself as well as the caller.
+func fatal(c cond) {
+	c.Skip = c.Skip + 2
+	c.Fataler.Fatal(c.String())
+}
+
+// Err ensures the error satisfies the given regular expression.
+func Err(t Fataler, err error, re *regexp.Regexp, a ...interface{}) {
+	if err == nil && re == nil {
+		return
+	}
+
+	if err == nil && re != nil {
+		fatal(cond{
+			Fataler:    t,
+			Format:     `expected error: "%s" but got a nil error`,
+			FormatArgs: []interface{}{re},
+			Extra:      a,
+		})
+		return
+	}
+
+	if err != nil && re == nil {
+		fatal(cond{
+			Fataler:    t,
+			Format:     `unexpected error: %s`,
+			FormatArgs: []interface{}{err},
+			Extra:      a,
+		})
+		return
+	}
+
+	if !re.MatchString(err.Error()) {
+		fatal(cond{
+			Fataler:    t,
+			Format:     `expected error: "%s" but got "%s"`,
+			FormatArgs: []interface{}{re, err},
+			Extra:      a,
+		})
+	}
+}
+
+// DeepEqual ensures actual and expected are equal. It does so using
+// reflect.DeepEqual.
+func DeepEqual(t Fataler, actual, expected interface{}, a ...interface{}) {
+	if !reflect.DeepEqual(actual, expected) {
+		fatal(cond{
+			Fataler:    t,
+			Format:     "expected these to be equal:\nACTUAL:\n%s\nEXPECTED:\n%s",
+			FormatArgs: []interface{}{spew.Sdump(actual), tsdump(expected)},
+			Extra:      a,
+		})
+	}
+}
+
+// NotDeepEqual ensures actual and expected are not equal. It does so using
+// reflect.DeepEqual.
+func NotDeepEqual(t Fataler, actual, expected interface{}, a ...interface{}) {
+	if reflect.DeepEqual(actual, expected) {
+		fatal(cond{
+			Fataler:    t,
+			Format:     "expected two different values, but got the same:\n%s",
+			FormatArgs: []interface{}{tsdump(actual)},
+			Extra:      a,
+		})
+	}
+}
+
+// Subset ensures actual matches subset.
+func Subset(t Fataler, actual, subset interface{}, a ...interface{}) {
+	if !subsetp.Check(subset, actual) {
+		fatal(cond{
+			Fataler:    t,
+			Format:     "expected subset not found:\nACTUAL:\n%s\nEXPECTED SUBSET\n%s",
+			FormatArgs: []interface{}{spew.Sdump(actual), tsdump(subset)},
+			Extra:      a,
+		})
+	}
+}
+
+// Nil ensures v is nil.
+func Nil(t Fataler, v interface{}, a ...interface{}) {
+	vs := tsdump(v)
+	sp := " "
+	if strings.Contains(vs[:len(vs)-1], "\n") {
+		sp = "\n"
+	}
+
+	if v != nil {
+		// Special case errors for prettier output.
+		if _, ok := v.(error); ok {
+			fatal(cond{
+				Fataler:    t,
+				Format:     `unexpected error: %s`,
+				FormatArgs: []interface{}{v},
+				Extra:      a,
+			})
+		} else {
+			fatal(cond{
+				Fataler:    t,
+				Format:     "expected nil value but got:%s%s",
+				FormatArgs: []interface{}{sp, vs},
+				Extra:      a,
+			})
+		}
+	}
+}
+
+// NotNil ensures v is not nil.
+func NotNil(t Fataler, v interface{}, a ...interface{}) {
+	if v == nil {
+		fatal(cond{
+			Fataler: t,
+			Format:  "expected a value but got nil",
+			Extra:   a,
+		})
+	}
+}
+
+// StringContains ensures string s contains the string substr.
+func StringContains(t Fataler, s, substr string, a ...interface{}) {
+	if !strings.Contains(s, substr) {
+		fatal(cond{
+			Fataler:    t,
+			Format:     `expected substring "%s" was not found in "%s"`,
+			FormatArgs: []interface{}{substr, s},
+			Extra:      a,
+		})
+	}
+}
+
+// StringDoesNotContain ensures string s does not contain the string substr.
+func StringDoesNotContain(t Fataler, s, substr string, a ...interface{}) {
+	if strings.Contains(s, substr) {
+		fatal(cond{
+			Fataler:    t,
+			Format:     `substring "%s" was not supposed to be found in "%s"`,
+			FormatArgs: []interface{}{substr, s},
+			Extra:      a,
+		})
+	}
+}
+
+// tsdump is Sdump without the trailing newline.
+func tsdump(a ...interface{}) string {
+	return strings.TrimSpace(spew.Sdump(a...))
+}
+
+// pstack is the stack upto the Test function frame.
+func pstack(s stack.Stack) string {
+	first := s[0]
+	if isTestFrame(first) {
+		return fmt.Sprintf("%s:%d: ", filepath.Base(first.File), first.Line)
+	}
+	var snew stack.Stack
+	for _, f := range s {
+		snew = append(snew, f)
+		if isTestFrame(f) {
+			return "        " + snew.String() + "\n"
+		}
+	}
+	return "        " + s.String() + "\n"
+}
+
+func isTestFrame(f stack.Frame) bool {
+	return strings.HasPrefix(f.Name, "Test")
+}
---
+++ ensure_test.go
@@ -0,0 +1,203 @@
+package ensure_test
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/facebookgo/ensure"
+)
+
+var log = os.Getenv("ENSURE_LOG") == "1"
+
+type capture struct {
+	bytes.Buffer
+}
+
+func (c *capture) Fatal(a ...interface{}) {
+	fmt.Fprint(&c.Buffer, a...)
+}
+
+var equalPrefix = strings.Repeat("\b", 20)
+
+func (c *capture) Equal(t testing.TB, expected string) {
+	// trim the deleteSelf '\b' prefix
+	actual := strings.TrimLeft(c.String(), "\b")
+	ensure.DeepEqual(t, actual, expected)
+	if log && expected != "" {
+		t.Log(equalPrefix, expected)
+	}
+}
+
+func TestNilErr(t *testing.T) {
+	var c capture
+	e := errors.New("foo")
+	ensure.Err(&c, e, nil)
+	c.Equal(t, "ensure_test.go:40: unexpected error: foo")
+}
+
+func TestMatchingError(t *testing.T) {
+	var c capture
+	e := errors.New("foo")
+	ensure.Err(&c, e, regexp.MustCompile("bar"))
+	c.Equal(t, "ensure_test.go:47: expected error: \"bar\" but got \"foo\"")
+}
+
+type typ struct {
+	Answer int
+}
+
+func TestExtras(t *testing.T) {
+	var c capture
+	e := errors.New("foo")
+	ensure.Err(
+		&c,
+		e,
+		nil,
+		map[string]int{"answer": 42},
+		"baz",
+		43,
+		44.45,
+		typ{Answer: 46},
+	)
+	c.Equal(t, `ensure_test.go:76: unexpected error: foo
+(map[string]int) {
+ (string) "answer": (int) 42
+}
+(string) "baz"
+(int) 43
+(float64) 44.45
+(ensure_test.typ) {
+ Answer: (int) 46
+}`)
+}
+
+func TestDeepEqualStruct(t *testing.T) {
+	var c capture
+	actual := typ{Answer: 41}
+	expected := typ{Answer: 42}
+	ensure.DeepEqual(&c, actual, expected)
+	c.Equal(t, `ensure_test.go:93: expected these to be equal:
+ACTUAL:
+(ensure_test.typ) {
+ Answer: (int) 41
+}
+
+EXPECTED:
+(ensure_test.typ) {
+ Answer: (int) 42
+}`)
+}
+
+func TestDeepEqualString(t *testing.T) {
+	var c capture
+	ensure.DeepEqual(&c, "foo", "bar")
+	c.Equal(t, `ensure_test.go:104: expected these to be equal:
+ACTUAL:
+(string) "foo"
+
+EXPECTED:
+(string) "bar"`)
+}
+
+func TestNotDeepEqualStruct(t *testing.T) {
+	var c capture
+	v := typ{Answer: 42}
+	ensure.NotDeepEqual(&c, v, v)
+	c.Equal(t, `ensure_test.go:114: expected two different values, but got the same:
+(ensure_test.typ) {
+ Answer: (int) 42
+}`)
+}
+
+func TestSubsetStruct(t *testing.T) {
+	var c capture
+	ensure.Subset(&c, typ{}, typ{Answer: 42})
+	c.Equal(t, `ensure_test.go:129: expected subset not found:
+ACTUAL:
+(ensure_test.typ) {
+ Answer: (int) 0
+}
+
+EXPECTED SUBSET
+(ensure_test.typ) {
+ Answer: (int) 42
+}`)
+}
+
+func TestUnexpectedNilErr(t *testing.T) {
+	var c capture
+	ensure.Err(&c, nil, regexp.MustCompile("bar"))
+	c.Equal(t, "ensure_test.go:135: expected error: \"bar\" but got a nil error")
+}
+
+func TestNilString(t *testing.T) {
+	var c capture
+	ensure.Nil(&c, "foo")
+	c.Equal(t, "ensure_test.go:141: expected nil value but got: (string) \"foo\"")
+}
+
+func TestNilInt(t *testing.T) {
+	var c capture
+	ensure.Nil(&c, 1)
+	c.Equal(t, "ensure_test.go:147: expected nil value but got: (int) 1")
+}
+
+func TestNilStruct(t *testing.T) {
+	var c capture
+	ensure.Nil(&c, typ{})
+	c.Equal(t, `ensure_test.go:156: expected nil value but got:
+(ensure_test.typ) {
+ Answer: (int) 0
+}`)
+}
+
+func TestNonNil(t *testing.T) {
+	var c capture
+	ensure.NotNil(&c, nil)
+	c.Equal(t, `ensure_test.go:162: expected a value but got nil`)
+}
+
+func TestStringContains(t *testing.T) {
+	var c capture
+	ensure.StringContains(&c, "foo", "bar")
+	c.Equal(t, "ensure_test.go:168: expected substring \"bar\" was not found in \"foo\"")
+}
+
+func TestStringDoesNotContain(t *testing.T) {
+	var c capture
+	ensure.StringDoesNotContain(&c, "foo", "o")
+	c.Equal(t, "ensure_test.go:174: substring \"o\" was not supposed to be found in \"foo\"")
+	if log {
+		t.Log("foo")
+	}
+}
+
+func TestExpectedNilErr(t *testing.T) {
+	var c capture
+	ensure.Err(&c, nil, nil)
+	c.Equal(t, "")
+}
+
+func indirect(f ensure.Fataler) {
+	ensure.StringContains(f, "foo", "bar")
+}
+
+func TestIndirectStackTrace(t *testing.T) {
+	var c capture
+	indirect(&c)
+	c.Equal(t, `        github.com/facebookgo/ensure/ensure_test.go:188 indirect
+github.com/facebookgo/ensure/ensure_test.go:195 TestIndirectStackTrace
+expected substring "bar" was not found in "foo"`)
+}
+
+func TestNilErrUsingNil(t *testing.T) {
+	var c capture
+	e := errors.New("foo")
+	ensure.Nil(&c, e)
+	c.Equal(t, "ensure_test.go:202: unexpected error: foo")
+}
